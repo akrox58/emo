4266b309b38989d60fd8bce0a929d8926d20da0c
akrox58 <rockit_gurl@yahoo.com>
2015-02-14 14:39:14 +0530

    Proper foldering

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Emo
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+   config.assets.enabled = true 
+config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
+   config.active_record.raise_in_transactional_callbacks = true
   end
 end
